ac064fe630627a91f7dc602dd65d2ac468ee9bcb
violet-anima <violet_anima@zoho.com>
2017-05-18 17:46:59 +0000

    first commit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+  $('.new-tweet form').on('keydown', 'textarea', function(event) {
+    var count = $(this).parent().find('.counter');
+    var counter = 140 - $(this).val().length;
+    count.html(counter);
+      if(counter >= 0) {
+        $(count).css('color', 'black');
+         } else {
+           $(count).css('color', 'red');
+        }
+  });
+});
+
+
+
