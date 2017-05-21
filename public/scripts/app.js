fd8e55a15b6b2752fac97fcd0a8916c45991ef3a
violet-anima <violet_anima@zoho.com>
2017-05-21 03:14:30 +0000

    deleted commmented out code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,22 +7,6 @@
 
 $(function () {
 
-  // var tweetData = {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": {
-  //       "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //       "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //     },
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  // };
-
 
   $("#tweet-form").keypress(function (e) {
     if (e.which == 13 && !e.shiftKey) {
